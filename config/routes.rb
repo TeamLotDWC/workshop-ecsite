11e362f0a945aacb73a5c6073e5b04fb66b0eb29
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2022-01-17 03:53:53 +0000

    initial push

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,44 @@
 Rails.application.routes.draw do
 
-
 devise_for :customers,skip: [:passwords,], controllers: {
-  registrations: "public/registrations",
+  registrations: 'public/registrations',
   sessions: 'public/sessions'
 }
 
-
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
+  sessions: 'admin/sessions'
 }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
+  get 'about' => 'homes#about'
 
-
-  resource :customers, only: [:show, :edit, :update] do
-    get "" => "#"
-    get "" => "#"
-    resources :shipping_addresses, only: [:index, :edit ,:create, :update, :destroy]
+  scope module: :public do
+    resource :customers, only: [:show, :edit, :update] do
+      collection do
+        patch 'delete'
+        get 'confirm_delete'
+      end
+      resources :shipping_addresses, only: [:index, :edit ,:create, :update, :destroy]
+      resources :cart_items, only: [:index, :create, :update, :destroy] do
+        delete 'reset', on: :collection
+      end
+      resources :orders, only:[:new, :index, :show, :create] do
+        collection do
+          get 'confirm'
+          get 'complete'
+        end
+      end
+    end
+    resources :items, only: [:index, :show]
   end
-
-
-
-
 
   namespace :admin do
     resources :customers, only: [:index,:show,:edit,:update]
-    resources :items, only: [:index,:new,:create,:show,:edit,:update,]
+    resources :items
     resources :genres, only: [:index,:create,:edit,:update, :show]
     resources :orders, only: [:index,:show,:update] do
-      member do
-          resource :order_items,only: [:update]
-      end
+      resources :order_items, only: [:update]
     end
   end
 
--- db/migrate/20220116064255_create_cart_items.rb
+++ db/migrate/20220116064255_create_cart_items.rb
@@ -3,7 +3,7 @@ class CreateCartItems < ActiveRecord::Migration[5.2]
     create_table :cart_items do |t|
       t.references :customer, foreign_key: true, null: false
       t.references :item, foreign_key: true, null: false
-      t.integer :quantity, null: false
+      t.integer :quantity, null: false, default: 0
       t.timestamps
       t.index [:customer_id, :item_id], unique: true
     end
