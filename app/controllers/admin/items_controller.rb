e4496fb825896cd88ed6b92df3956f06351ad712
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2022-01-18 08:39:07 +0000

    作業中断

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,47 @@
 class Admin::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.update(item_params)
+    if @item.update(item_params)
+      redirect_to admin_item_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to admin_items_path
+  end
+
+  private
+  def
+    params.require(:item).permit(:name, :introduction, :item_image, :genre, :net_price, :is_stocked)
+  end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,11 @@
 class Public::ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+  
 end
