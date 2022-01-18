4480cc39fdde130dab0bb8d60473957c5656c728
EC2 Default User <ec2-user@ip-172-31-16-246.ec2.internal>
2022-01-18 03:05:36 +0000

    make customer expect edit

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -19,6 +20,15 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   # protected
+  
+  def customer_state
+    @customer = Customer.find_by(email: paramus[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password])
+      if @customer.is_deleted == true
+        redirect_to new_customer_registration
+      end
+    e
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
