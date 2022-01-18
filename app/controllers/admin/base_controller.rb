f7b8637013cf479369d64f37d8503c6d0a0c8584
Nedhida <pinedoutshadeyouth@gmail.com>
2022-01-18 00:15:17 +0000

    [Unavailable]adminCustomerUpdate[Available]otherAdminCustomers

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,5 @@
 class Admin::BaseController < ApplicationController
 
-    def full_name
-        self.first_name + self.family_name
-    end
-
-    def full_name_kana
-        self.first_name_kana + self.family_name_kana
-    end
+    before_action :authenticate_admin!
 
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -20,6 +20,14 @@ class Public::SessionsController < Devise::SessionsController
 
   # protected
 
+  def after_sign_in_path_for(resource)
+    customers_orders_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_customer_session_path
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
