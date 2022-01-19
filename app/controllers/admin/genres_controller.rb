74d4e359015a92f0bd6c668118d55f0fd23aab9c
Nedhida <pinedoutshadeyouth@gmail.com>
2022-01-19 04:43:03 +0000

    [Fix]aroundFormWith

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -9,7 +9,7 @@ class Admin::GenresController < ApplicationController
   def create
     @genre = Genre.new(genre_params)
     if @genre.save
-      redirect_to admin_genres_path, status: 301
+      redirect_to admin_genres_path
     else
       @genres = Genre.all
       render 'index'
