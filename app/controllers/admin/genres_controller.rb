b9718e29a795c2d124713597f0ea7a4d30d92ef7
Nedhida <pinedoutshadeyouth@gmail.com>
2022-01-19 06:24:34 +0000

    [Fix]model

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -20,7 +20,7 @@ class Admin::GenresController < ApplicationController
   end
 
   def update
-    if @genre.update!(genre_params)
+    if @genre.update(genre_params)
       redirect_to admin_genres_path
     else
       render 'edit'
