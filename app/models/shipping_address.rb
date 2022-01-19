db6621d1cc7e64854bc78018ee31957eb57738af
Nedhida <pinedoutshadeyouth@gmail.com>
2022-01-19 05:00:57 +0000

    [Fix]aroundFromWith

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,8 +1,8 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :customer, optional: true
 
-  validates :zip_code, presence: true, length: {minimum: 7, maximum: 7}
-  validates :address, presence: true, uniqueness: true, length: {minimum: 6}
-  validates :name, presence: true, length: {minimum: 2}
+  validates :zip_code, length: {minimum: 7, maximum: 7}
+  validates :address, uniqueness: true, length: {minimum: 6}
+  validates :name, length: {minimum: 2}
 
 end
