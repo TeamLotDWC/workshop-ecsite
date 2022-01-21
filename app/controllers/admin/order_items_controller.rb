58eeaa9c44979ab7f02a600f760ff6e74d7cbc43
EC2 Default User <ec2-user@ip-172-31-16-246.ec2.internal>
2022-01-21 06:34:04 +0000

    admin order end

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,19 @@
 class Admin::OrderItemsController < ApplicationController
+  
+  def update
+    @order_item = OrderItem.find(params[:id])
+    @order = @order_item.order
+    if @order_item.update(order_item_params)
+      redirect_to admin_order_path(@order), notice: 'sucsess'
+    else
+      @order = @order_item.order
+      render 'admin/order/show'
+    end
+  end
+  
+  private
+  
+  def order_item_params
+    params.require(:order_item).permit(:process_status)
+  end
 end
